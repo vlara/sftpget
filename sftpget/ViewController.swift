a46ccd89fedb75316d2a09fb637c952f9ef875da
Victor Lara <lara.m.victor@gmail.com>
2015-07-12 12:07:49 -0700

    Initial Commit

---
+++ sftpget/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sftpget
+//
+//  Created by Victor Lara on 7/12/15.
+//  Copyright (c) 2015 Victor Lara. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
